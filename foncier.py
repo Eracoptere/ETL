40bd858113b481b3ba0407c40d2abcad6a04f8e1
Al Rei <allan.reichmuth@gmail.com>
2022-07-04 16:25:27 +0200

    v2/Un peu plus automatisé par utilisation du scrapping

--- foncier.py
+++ foncier.py
@@ -1,74 +1,56 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
+"""Extrait un csv depuis une url data.gouv les données foncières de l'année publiée la plus récente puis transforme les données brutes en
+un dataframe conforme à la volonté du client"""
 
 import pandas as pd
-import numpy as np
-
-
-# In[2]:
-
+from bs4 import BeautifulSoup
+import requests
 
 #url de la source de données 
-#url_foncier="https://www.data.gouv.fr/fr/datasets/demandes-de-valeurs-foncieres/"
+url_foncier="https://www.data.gouv.fr/fr/datasets/demandes-de-valeurs-foncieres/"
 
+#Extraction du code html de la page/Obtention du dernier lien en date
+response = requests.get(url_foncier)
+soup=BeautifulSoup(response.text)
+target=soup.find("li", {"class":"fr-col-auto"}).find('a')
+source_foncier=target.get('href')
 
-# In[3]:
+#Extraction du csv et import des données
+foncier_csv_link=requests.get(source_foncier)
+local_path_foncier='C:/Users/utilisateur/Documents/ETL/foncier_base.csv'
+with open(local_path_foncier, "wb") as f:
+    f.write(foncier_csv_link.content)
+    
+df0_foncier=pd.read_csv(local_path_foncier, sep='|')
+print("Voici les données extraites:", df0_foncier)
 
-
-#Import des données
-df0_foncier=pd.read_csv('./Simplon/PG DE/valeursfoncieres-2021.txt', sep='|')
-
-
-# In[4]:
-
-
-
+#Copie du fichier d'origine
 df_foncier=df0_foncier.copy()
-print(df_foncier.shape)
-print(df_foncier.isna().sum())
 
-
-# In[5]:
-
+#print(df_foncier.shape)
+#print(df_foncier.isna().sum())
 
 #Colonne à supprimer
-df_foncier.drop(columns=["Code service CH", "Reference document","1 Articles CGI","2 Articles CGI","3 Articles CGI",
-                         "4 Articles CGI","5 Articles CGI","Code type local","No Volume","1er lot","Surface Carrez du 1er lot",
-                         "2eme lot","Surface Carrez du 2eme lot","3eme lot","Surface Carrez du 3eme lot","4eme lot","Surface Carrez du 4eme lot",
-                         "5eme lot","Surface Carrez du 5eme lot","Section","No plan","Nombre de lots",
-                         "Section","No plan","Identifiant local","No disposition","Code departement","Code commune",
-                         "Nature culture","Nature culture speciale","Code voie","Prefixe de section"], axis=1, inplace=True)
-
-
-# In[6]:
-
+df_foncier=df_foncier[['Date mutation', 'Nature mutation', 'Valeur fonciere', 'No voie',
+       'B/T/Q', 'Type de voie', 'Voie', 'Code postal', 'Commune', 'Type local',
+       'Surface reelle bati', 'Nombre pieces principales', 'Surface terrain']]
 
 #Liste des valeurs à convertir en float
 int_list=["Valeur fonciere","No voie","Code postal","Surface reelle bati","Nombre pieces principales","Surface terrain"]
 
-
-# In[7]:
-
-
 #Remplacement des virgules par des points pour la colonne "Valeurs fonciere"
 df_foncier['Valeur fonciere']=df_foncier['Valeur fonciere'].str.replace(',','.')
-
-
-# In[8]:
 
 
 #Conversion de object vers float pour les colonnes concernées
 for colonne in int_list:
     df_foncier[colonne]=df_foncier[colonne].astype(float)
 
-
-# In[12]:
-
-
 #Changement de format de date
 df_foncier['Date mutation']=pd.to_datetime(df_foncier['Date mutation'])
-print(df_foncier)
 
+
+#Sauvegarde des données transformées 
+df_foncier.to_csv('C:/Users/utilisateur/Documents/ETL/foncier_transform.csv')
