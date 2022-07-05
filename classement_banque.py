538d07ee5feffe5177b5ddc721b69e8f21439078
Al Rei <allan.reichmuth@gmail.com>
2022-07-05 14:54:19 +0200

    Fichiers inutiles supprimés

--- classement_banque.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import pandas as pd
-url = "https://en.wikipedia.org/wiki/List_of_largest_banks"
-dfs = pd.read_html(url)
-df = dfs[3]
-df.to_csv('donnees_banque.csv')
